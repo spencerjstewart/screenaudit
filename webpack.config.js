537379581e4f752e18a28bf86d55b290f677fb6d
Spencer Stewart <spencerjamesstewart@gmail.com>
2023-12-29 02:43:38 -0600

    Update webpack config for basic first functionality tests.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,33 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js', // Path to your main JavaScript file
-  output: {
-    filename: 'background.js', // Output bundle file name
-    path: path.resolve(__dirname, 'dist'), // Output directory
+  entry: {
+    background: './src/background.js',
+    options: './src/options/options.js',
+    popup: './src/popup/popup.js',
   },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'manifest.json', to: 'manifest.json' },
+        { from: 'src/options/options.html', to: 'options.html' },
+        { from: 'src/popup/popup.html', to: 'popup.html' },
+        // you can also copy other assets in a similar manner
+      ]
+    }),
+    new Dotenv()
+  ],
+  module: {
+    rules: [
+      // Rules for CSS, and other file types
+    ]
+  },
+  mode: 'development', // Or 'production'
+  devtool: 'inline-source-map', // Remove for production
 };
